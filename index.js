9a40b407bbcfcbc62a4ffd9195fd219d8b0836a3
Evaloper <gbajeeva@yahoo.com>
2023-02-03 12:18:24 +0100

    Counter

---
+++ index.js
@@ -0,0 +1,50 @@
+// Declare a variable for count and initialize it to 0.
+
+
+
+let count = 0;
+let countNumber = document.getElementById("number-count");
+let sum = Number(countNumber.innerHTML) + count;
+
+// A function for every click on the increment button increase count by 1
+
+function increment() {
+    count += 1;
+    countNumber.innerHTML = count;
+    
+    
+}
+// A function for every click on the increment button increase count by 1
+
+function decrement() {
+    count -= 1;
+    countNumber.innerHTML = count;
+}
+// A function for every click on the save button and it saves/ adds the incremented number to the entries paragraph
+
+let saveCount = document.getElementById("save-btn")
+
+function save() {
+    let countStr = document.getElementById("para");
+    countStr.textContent += count + " - ";
+    countNumber.innerHTML = 0;
+    count = 0; 
+    
+}
+
+let totalPara = document.getElementById("total");
+
+function add() {
+    
+
+    // let add = count + count
+    // add.textContent = count;
+    // countNumber.innerHTML = 0;
+    // count = 0;
+    // add.textContent = count;
+    
+    // alert(add)
+    // return add
+    // sum.textContent = totalPara;
+}
+
